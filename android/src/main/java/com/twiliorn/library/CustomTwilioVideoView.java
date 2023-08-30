4fda9be1f29e5491502f6a872739c83eeed1e5d1
Dain Finn <dain.finn@streem.com>
2023-08-30 10:03:02 -0700

    Bump version to 3.2.3

--- android/src/main/java/com/twiliorn/library/CustomTwilioVideoView.java
+++ android/src/main/java/com/twiliorn/library/CustomTwilioVideoView.java
@@ -490,7 +490,7 @@ public class CustomTwilioVideoView extends View implements LifecycleEventListene
     }
 
     public void connectToRoom() {
-        Log.d("RNTwilioVideo", "connectToRoom: ST-3629 test, updated log message");
+        Log.d("RNTwilioVideo", "connectToRoom: ST-3629 test, updated log message version 3.2.3");
 
         /*
          * Create a VideoClient allowing you to connect to a Room
